cd5421edc4ac91f454a22bafc6537fcb5ca9371e
Eyal-donut <LilaEyal@gmail.com>
2023-04-03 21:24:02 +0300

    workinggggg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState, useRef } from "react";
+import { BrowserBarcodeReader } from "@zxing/library";
 
 function App() {
+  const [selectedDeviceId, setSelectedDeviceId] = useState(null);
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const codeReader = new BrowserBarcodeReader();
+    console.log("ZXing code reader initialized");
+
+    codeReader
+      .getVideoInputDevices()
+      .then((videoInputDevices) => {
+        setSelectedDeviceId(videoInputDevices[0].deviceId);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  const handleSourceChange = (event) => {
+    setSelectedDeviceId(event.target.value);
+  };
+
+  const handleStart = () => {
+    const codeReader = new BrowserBarcodeReader();
+    codeReader.decodeFromVideoDevice(
+      selectedDeviceId,
+      videoRef.current,
+      (result, error) => {
+        if (result) {
+          console.log(result);
+          document.getElementById("result").textContent = result.getText();
+        }
+        if (error && error.constructor.name !== "NoVideoInputDevicesError") {
+          // console.error(error);
+          document.getElementById("result").textContent = error;
+        }
+      }
+    );
+
+    console.log(
+      `Started continuous decode from camera with id ${selectedDeviceId}`
+    );
+  };
+
+  const handleReset = () => {
+    document.getElementById("result").textContent = "";
+    const codeReader = new BrowserBarcodeReader();
+    codeReader.reset();
+    console.log("Reset.");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="sourceSelectPanel" style={{ display: "none" }}>
+        <select id="sourceSelect" onChange={handleSourceChange}></select>
+      </div>
+      <button id="startButton" onClick={handleStart}>
+        Start
+      </button>
+      <button id="resetButton" onClick={handleReset}>
+        Reset
+      </button>
+      <div id="result"></div>
+      <video ref={videoRef} style={{ display: "block" }} autoPlay></video>
     </div>
   );
 }
