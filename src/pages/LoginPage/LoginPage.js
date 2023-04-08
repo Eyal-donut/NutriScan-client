2a0358e5fa88b38031ae9516ce02deb1436044fa
Eyal-donut <LilaEyal@gmail.com>
2023-04-08 18:16:46 +0300

    loginForm

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,33 +1,27 @@
-// import { useState } from "react";
 import Modal from "../../components/Modal/Modal";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import { loginUser } from "../../api";
-// import { useState } from "react";
 import { useErrorMessageContext } from "../../context/ErrorMessageContext";
 
 const LoginPage = () => {
-  const {setErrorMessage} = useErrorMessageContext()
-
-  // const [isLoginFailed, setIsLoginFailed] = useState(false);
-  // const [loginMessage, setLoginMessage] = useState("")
+  const { setErrorMessage } = useErrorMessageContext();
 
   const loginHandler = async (e, email, password) => {
     e.preventDefault();
     console.log(email, password);
-    const data = await loginUser({email, password})
+    const data = await loginUser({ email, password });
     if (data.success) {
-      console.log(data.token)
+      console.log(data.token);
     } else {
-      console.error(data.error)
-      setErrorMessage(data.error)
-      // setIsLoginFailed(true)
+      console.error(data.error);
+      setErrorMessage(data.error);
     }
   };
 
   return (
     <Modal>
       <h1>Log in</h1>
-      <LoginForm loginHandler={loginHandler}/>
+      <LoginForm loginHandler={loginHandler} />
     </Modal>
   );
 };
