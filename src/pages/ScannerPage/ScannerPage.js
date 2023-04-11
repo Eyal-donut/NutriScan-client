f3a6b82f984c11e4dea92ca3fcf0bd491d1ac316
Eyal-donut <LilaEyal@gmail.com>
2023-04-11 12:53:06 +0300

    scanner_quagga

--- src/pages/ScannerPage/ScannerPage.js
+++ src/pages/ScannerPage/ScannerPage.js
@@ -1,14 +1,15 @@
 import Scanner from "../../components/Scanner/Scanner";
 
 const ScannerPage = () => {
-
-  const handleScan = (barcode) => {
+  const handleDetected = (barcode) => {
     console.log(barcode);
   };
   return (
     <>
       <h1>Scanner Page</h1>
-      <Scanner onDetectedBarcode={handleScan} />
+      <div className="scannerContainer">
+        <Scanner onDetectedBarcode={handleDetected} />
+      </div>
     </>
   );
 };
