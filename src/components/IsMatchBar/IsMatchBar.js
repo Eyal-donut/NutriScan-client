307004b3c6107db4d1b528132810984762be7e13
Eyal-donut <LilaEyal@gmail.com>
2023-04-12 15:28:27 +0300

    isMatchBar

--- src/components/IsMatchBar/IsMatchBar.js
+++ src/components/IsMatchBar/IsMatchBar.js
@@ -2,8 +2,20 @@ import classes from "./IsMatchBar.module.css";
 
 const IsMatchBar = (isMatch) => {
   return (
-    <div className={isMatch ? classes.containerYes : classes.containerNo}>
-      {isMatch ? `It's a match!` : `Not a matching your preferences`}
+    <div
+      className={
+        isMatch === "yes"
+          ? classes.containerYes
+          : isMatch === "no"
+          ? classes.containerNo
+          : classes.containerUnknown
+      }
+    >
+      {isMatch === "yes"
+        ? `It's a match!`
+        : isMatch === "no"
+        ? `Not a matching your preferences`
+        : `Unknown`}
     </div>
   );
 };
--- src/pages/ProductPage/ProductPage.js
+++ src/pages/ProductPage/ProductPage.js
@@ -5,6 +5,7 @@ import ProductCardMain from "../../components/ProductCardMain/ProductCardMain";
 import { useProductContext } from "../../context/ProductContext";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
 import IsMatchBar from "../../components/IsMatchBar/IsMatchBar";
+import ContentWrap from "../../components/ContentWrap/ContentWrap";
 
 const ProductPage = () => {
   const { setCurrentProduct } = useProductContext();
@@ -13,14 +14,14 @@ const ProductPage = () => {
   useEffect(() => {
     const currentProductLocal = getLocalStorageItem("currentProduct");
     setCurrentProduct(currentProductLocal);
-    
+
     // eslint-disable-next-line
   }, []);
 
   return (
     <>
       <h1>Product Page</h1>
-      <IsMatchBar/>
+      <IsMatchBar />
       <ProductCardMain />
     </>
   );
