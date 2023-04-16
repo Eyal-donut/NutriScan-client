f137c73df353354ad0e8e3dd0717360ae462d52f
Eyal-donut <LilaEyal@gmail.com>
2023-04-16 19:58:52 +0300

    convertNutValues

--- src/hooks/useUserSettings/utils/convertNutValuesToClasses.js
+++ src/hooks/useUserSettings/utils/convertNutValuesToClasses.js
@@ -8,13 +8,13 @@ export const convertNutValuesToClasses = (name, value) => {
     switch (name) {
       case "Carbohydrates":
         if (value === 0) return 1;
-        if (value < nutValues.LOW_CARBS) return 2;
+        if (value < nutValues.LOW_CARBS && value > 0) return 2;
         if (value >= nutValues.LOW_CARBS && value <= nutValues.MODERATE_CARBS)
           return 3;
         else return 4;
       case "Cholesterol":
         if (value === 0) return 1;
-        if (value < nutValues.LOW_CHOLESTEROL) return 2;
+        if (value < nutValues.LOW_CHOLESTEROL && value > 0) return 2;
         if (
           value >= nutValues.LOW_CHOLESTEROL &&
           value <= nutValues.MODERATE_CHOLESTEROL
@@ -23,13 +23,13 @@ export const convertNutValuesToClasses = (name, value) => {
         else return 4;
       case "Fat":
         if (value === 0) return 1;
-        if (value < nutValues.LOW_FAT) return 2;
+        if (value < nutValues.LOW_FAT && value > 0) return 2;
         if (value >= nutValues.LOW_FAT && value <= nutValues.MODERATE_FAT)
           return 3;
         else return 4;
       case "Saturated fat":
         if (value === 0) return 1;
-        if (value < nutValues.LOW_SAT_FAT) return 2;
+        if (value < nutValues.LOW_SAT_FAT && value > 0) return 2;
         if (
           value >= nutValues.LOW_SAT_FAT &&
           value <= nutValues.MODERATE_SAT_FAT
@@ -38,13 +38,13 @@ export const convertNutValuesToClasses = (name, value) => {
         else return 4;
       case "Salt":
         if (value === 0) return 1;
-        if (value < nutValues.LOW_SALT) return 2;
+        if (value < nutValues.LOW_SALT && value > 0) return 2;
         if (value >= nutValues.LOW_SALT && value <= nutValues.MODERATE_SALT)
           return 3;
         else return 4;
       case "Sugar":
         if (value === 0) return 1;
-        if (value < nutValues.LOW_SUGAR) return 2;
+        if (value < nutValues.LOW_SUGAR && value > 0) return 2;
         if (value >= nutValues.LOW_SUGAR && value <= nutValues.MODERATE_SUGAR)
           return 3;
         else return 4;
