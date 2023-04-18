b7ace39c25bcb6daef70c3636489010ef4de2804
Eyal-donut <LilaEyal@gmail.com>
2023-04-18 19:22:10 +0300

    useUserSettings-updated

---
+++ src/hooks/useUserSettings/utils/getMatchFromAllResults.js
@@ -0,0 +1,10 @@
+export const getMatchFromResults = (allResults) => {
+    const categoriesMatchResults = allResults.map((res)=> res.isMatch)
+    const noMatch = categoriesMatchResults.find((res)=> res === false)
+    if (noMatch) return false
+    const unknown = categoriesMatchResults.find((res)=> res === "Unknown")
+    if(unknown) return "Unknown"
+    const noFilters = categoriesMatchResults.find((res)=> res === true)
+    if(noFilters) return true
+    return "no filters"
+  }
