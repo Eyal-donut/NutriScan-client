28ac9171750a6d5af035b43eafc27b2419f8d945
Eyal-donut <eyallila2@gmail.com>
2023-06-08 10:00:16 +0300

    indexChanges

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom/client';
 import "./index.css";
 import "./constants/styleConstants.css";
 import App from "./App";
@@ -12,8 +12,10 @@ import { ProductProvider } from "./context/ProductContext";
 import { CameraProvider } from "./context/CameraContext";
 import { MenuDisplayProvider } from "./context/MenuDisplayContext";
 
-const root: HTMLElement | null = document.getElementById("root");
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <LoginAndRegisterPagesProvider>
       <ErrorMessageProvider>
